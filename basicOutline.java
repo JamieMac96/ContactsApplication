4b687cc9c18eea691bab5464e50d7db9cfcae8a5
JamieMac96 <jamiemac96@hotmail.com>
2016-02-16 15:22:43 +0000

    Improved version of the main method outline

--- basicOutline.java
+++ basicOutline.java
@@ -1,62 +1,61 @@
-/*
-import java.util.Scanner;
-imprt java.io.*;
-public class P2Outline
+import java.util.*;
+import java.io.*;
+public class BasicOutline
 {
 	public static void main(String [] args) throws IOException
-	{
-		Scanner sc = new Scanner(System.in);
-		FileWriter aFileWriter = new FileWriter("Contacts.txt");
-		PrintWriter out = new PrintWriter(aFileWriter);
-		File Contacts = new File(fileName);
+	{	
+		String pattern = ".*\\.txt";
 		
-		String fileName = "contacts.txt";
-		String loadFile = "", createFile = "", firstContact = "", fromMenu = "";
-		String [] callType = {"i", "r", "e", "d", "s"};
-		
-		System.out.print("would you like to try to load a contacts file (y/Y/n/N)");
-		loadFile = sc.nextLine;
-		if(loadFile.equals("y"))
-		{
-			if(Contacts.exists())
+		if(args.length != 1)
+			System.out.print("Java Usage: java ContactsApp Anyfile.txt");//these check the user's input.
+		else if(!(args[0].matches(pattern)))
+			System.out.print("Your command line argument must be a .txt file.");
+		else
+		{	
+			Scanner sc = new Scanner(System.in);
+			String createFile = "", fromMenu = "";
+			File contacts = new File(args[0]);
+	
+			if(!contacts.exists())
 			{
-				System.out.print("File exists");
-			}
-			else
-			{
-				System.out.print("No file exists. Would you like to create one? (y/Y/n/N): ");
-				createFile = sc.nextLine();
+				System.out.print("No such file found. Would you like to create a file? (Y/y/N/n): ");
+				createFile = sc.nextLine();//validate()
 				if(createFile.equals("y"))
 				{
-					System.out.print("Enter your first contact to create the file: ");
-					firstContact = sc.nextLine();
-					out.print(firstContact + "\n");
+					FileWriter aFileWriter = new FileWriter(args[0]);
+					PrintWriter out = new PrintWriter(aFileWriter);
+					out.print("");
 					out.close();
-					aFileWriter.close(); (not sure if these should be closed here or later on)
+					aFileWriter.close(); //(not sure if these should be closed here or later on)
 				}
 			}
 			
-			System.out.print("i(nsert)\tr(emove)\te(dit)\nd(isplay)\ts(earch)\tq(uit)");
-			fromMenu = sc.nextLine();
-			
-			while(Contacts.exists() && !fromMenu.equals("q"))
-			{
-				if(fromMenu.equals("i"))
-					insert();
-				else if(fromMenu.equals("r"))
-					remove();
-				else if(fromMenu.equals("e"))
-					edit();
-				else if(fromMenu.equals("d"))
-					display();
-				else if(fromMenu.equals("s"))
-					search();
+			if(contacts.exists())//this is where the main part of the program begins.
+			{	
+				ArrayList<ArrayList<String>> contactInfo = new ArrayList<ArrayList<String>>();
+				for(int i = 0; i < 6; i++)
+					contactInfo.add(new ArrayList<String>());	//arrayList with 6 columns
 				
-				System.out.print("i(nsert)\tr(emove)\te(dit)\nd(isplay)\ts(earch)\tq(uit)");
-				fromMenu = sc.nextLine();
-			}
+				fromMenu = menuMethod();//validate(should probably call from the menuMethod itself)
+				
+				while(!fromMenu.equals("q"))
+				{	
+					if(fromMenu.equals("i"))
+						insert();
+					else if(fromMenu.equals("r"))
+						remove();
+					else if(fromMenu.equals("e"))
+						edit();
+					else if(fromMenu.equals("d"))
+						display();
+					else if(fromMenu.equals("s"))
+						search();
+			
+					fromMenu = menuMethod();//validate
+				}
+			}		
 		}
-	}
+	}	
 	
 	public static void insert()
 	{
@@ -89,4 +88,3 @@ public class P2Outline
 	}
 }
 
-*/
